2d2cfab48e2a74e1838c0b928e19ddf38fe4ab9e
Hendy Sung <hendysung@Hendy.local>
2025-02-05 14:11:51 +0700

    init

---
+++ exercise.ts
@@ -0,0 +1,5 @@
+let Test : string = "Coba-coba yuk";
+console.log(Test); 
+
+Test = "Ngantuk parah"
+console.log("Number"); 
---
+++ readme.md
@@ -0,0 +1,3 @@
+ini adalah sebuah repository untuk exercise GIT
+
+note: ini cuma tulisan atau notes doang.
